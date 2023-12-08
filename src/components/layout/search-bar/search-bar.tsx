963871148c428a18dac6d7c1edc77040efb802be
Hugo Dessomme <hdessomme@gmail.com>
2023-12-08 15:13:20 +0100

    fix: align vertically search bar

--- src/components/layout/search-bar/search-bar.tsx
+++ src/components/layout/search-bar/search-bar.tsx
@@ -9,7 +9,7 @@ function SearchBar() {
   return (
     <div className="border-b border-gray-6 bg-gray-1 py-4 shadow-sm dark:border-dark-gray-6 dark:bg-dark-gray-1">
       <div className="container">
-        <div className="flex justify-between">
+        <div className="flex items-center justify-between">
           <div className="flex items-center gap-x-10">
             <Link
               href="/"
